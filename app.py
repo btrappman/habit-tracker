70c1300f10df1fe41dd8b55abe17206c23712807
Brandon <btrappman@protonmail.com>
2022-03-21 11:24:13 -0700

    added stylesheet

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from distutils.log import debug
 from flask import Flask, render_template
 
 # Flask Convention
@@ -7,3 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+# TODO: Not sure this is required, interested to see how debugging works on render
+# looks like debugger is on only if run with python3 app.py (instead of flask run)
+if __name__ == "__main__":
+    app.run(debug=True)
